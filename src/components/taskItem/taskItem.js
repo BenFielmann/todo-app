ded17a0071601fcf16505c669c0516a53b65301c
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-07-26 12:09:06 +0200

    TaskItem

---
+++ src/components/taskItem/taskItem.js
@@ -0,0 +1,9 @@
+import "./taskItem.css";
+import taskItem from "./taskItem.html";
+
+export default {
+  title: "Components/TaskItem",
+  parameters: { layout: "fullscreen" },
+};
+
+export const taskItemExp = () => taskItem;
