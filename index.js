4fff3c004f5df2c577f6c3cc625618d80f72b241
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-07-27 17:00:02 +0200

    created new task item funktion

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-let message = "Hello World";
-message = "Hello Students";
+const actionbutton = document.querySelector(".action-button");
+actionbutton.onclick = createTaskListItem;
 
-alert(message);
+function sendAlert() {
+  alert("Hello World");
+}
+
+const taskList = document.querySelector(".taskList");
+
+const newTask = document.createElement("p");
+newTask.innerText = "Example Task";
+
+taskList.append(newTask);
+
+function createTaskListItem() {
+  const taskListItem = document.createElement("label");
+  taskListItem.className = "taskItem";
+
+  const taskItemInput = document.createElement("input");
+  taskItemInput.className = "taskItem__checkbox";
+  taskItemInput.setAttribute("type", "checkbox");
+
+  const taskItemText = document.createElement("span");
+  taskItemText.className = "taskItem__labelText";
+  taskItemText.innerText = "Example Task";
+
+  taskListItem.append(taskItemInput);
+  taskListItem.append(taskItemText);
+  taskList.append(taskListItem);
+
+  return taskListItem;
+}
