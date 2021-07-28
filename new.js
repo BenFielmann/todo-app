7df1e831bd33c99c30e23ce518e6096ba05f41dd
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-07-28 14:33:57 +0200

    function done

--- new.js
+++ new.js
@@ -1,25 +1,11 @@
-const altButton = document.querySelector(".addTaskForm__radio");
-
-altButton.onclick = alertSelectetDate;
-
-function alertSelectetDate() {
-  const checkedDateIput = document.querySelector(
-    ".addTaskForm__radioLabel:checked"
-  );
-  alert(checkedDateIput.value);
-}
-
 const form = document.querySelector(".addTaskForm");
 const formSubmit = document.querySelector(".addTaskForm__submit");
 
 form.onsubmit = function (event) {
-  // prevent the default stops reloading of the page//
   event.preventDefault();
-  const checkedDateIput = document.querySelector(
-    ".addTaskForm__radioLabel:checked"
-  );
-  const textInput = document.querySelector(".addTaskForm__textInput");
+  const checkedDateIput = document.querySelector(".addTaskForm__radio:checked")
+    .value;
+  const textInput = document.querySelector(".addTaskForm__textInput").value;
 
-  const selectdate
+  console.log(checkedDateIput, textInput);
 };
-
