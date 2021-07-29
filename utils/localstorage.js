4dd6ae7c87077cc986fb6862bcc4144411b8a216
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-07-29 17:34:56 +0200

    LocalStorage

--- utils/localstorage.js
+++ utils/localstorage.js
@@ -1,3 +1,14 @@
-export function parseJSONEFromLocalStorage(key){
+export function parseJSONFromLocalStorage(key, defaueltValue) {
+  const json = JSON.parsen(localStorage.getItem(key));
 
+  if (!json) {
+    return defaueltValue;
+  }
+
+  return json;
+}
+
+export function stringifyJSONToLocalStorage(key, value) {
+  const json = JSON.stringify(value);
+  localStorage.setItem(key, json);
 }
