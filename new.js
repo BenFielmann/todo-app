78dbfcdb025e310f38d411d0081f9caaa28a82be
BenFielmann <Benjamin.Fielmann@gmx.de>
2021-07-29 17:30:59 +0200

    new.js linked to localstorage.js

--- new.js
+++ new.js
@@ -1,41 +1,22 @@
+import {
+  parseJSONFromLocalStorage,
+  stringifyJSONToLocalStorage,
+} from "./utils/localstorage.js";
+
 const form = document.querySelector(".addTaskForm");
 const formSubmit = document.querySelector(".addTaskForm__submit");
 
 form.onsubmit = function (event) {
   event.preventDefault();
-  const checkedDateIput = document.querySelector(".addTaskForm__radio:checked")
-    .value;
-  const textInput = document.querySelector(".addTaskForm__textInput").value;
+  const taskObject = {
+    name: "",
+    date: "",
+    isDone: false,
+  };
+  taskObject.date = document.querySelector(".addTaskForm__radio:checked").value;
+  taskObject.name = document.querySelector(".addTaskForm__textInput").value;
 
-  console.log(checkedDateIput, textInput);
+  //console.log(checkedDateIput, textInput);
+
+  stringifyJSONToLocalStorage("task", taskObject);
 };
-
-
-
-
-// Display sleceted date onsubmit, using submit button
-const form = document.querySelector(".addTaskForm");
-
-form.onsubmit = function (event) {
-  // Prevent the default functionality of the submit event, which is reloading the page
-  event.preventDefault();
-
-  const newTask = { title: "", date: "", isDone: false };
-
-  const checkedDateInput = document.querySelector(
-    ".addTaskForm__radio:checked"
-  );
-  const textInput = document.querySelector(".addTaskForm__textInput");
-
-  newTask.title = textInput.value;
-  newTask.date = checkedDateInput.value;
-
-  // template literals
-  console.log(
-    `Hi you entered "${newTask.date}" as date and "${newTask.title}" as description`
-  );
-  console.log(newTask);
-};
-
-
-
